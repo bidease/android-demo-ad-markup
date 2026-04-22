712ef3e53e0d60fa0e0f53ff9a8b8bac4f6fe731
mkrasnorutsky <mkrasnorutsky@mail.ru>
2026-04-22 16:30:14 +0700

    ok

--- app/src/main/java/com/bidease/android/demo/admarkup/AdmBannerView.kt
+++ app/src/main/java/com/bidease/android/demo/admarkup/AdmBannerView.kt
@@ -5,9 +5,11 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import com.bidease.mobile.ads.AdService
 import com.bidease.mobile.ads.mraid.MraidEnv
 import com.bidease.mobile.ads.AdWebView
 import com.bidease.mobile.ads.AdSize
+import com.bidease.mobile.ads.AdView
 import com.bidease.mobile.ads.BideaseJsInterface
 import com.bidease.mobile.ads.MraidJsInterface
 import com.bidease.mobile.ads.MraidWebViewClient
@@ -29,12 +31,10 @@ import kotlin.String
 class AdmBannerView: FrameLayout {
     private val coroutineScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
     private val adViewController = AdViewController()
-    private val scriptSource = ScriptSource(context)
     private val webViewChannel = DefaultWebViewChannel()
 
     private var adSize: AdSize? = null
-    private var adViews: List<View> = emptyList()
-    private var isLoaded = false
+    private var sceneResults: List<AdView.LoadResult> = emptyList()
 
     var onLoaded: (() -> Unit)? = null
     var onDisplayed: (() -> Unit)? = null
@@ -64,87 +64,56 @@ class AdmBannerView: FrameLayout {
 
     suspend fun show(adSize: AdSize, scenes: List<Scene>?) {
         this.adSize = adSize
-        val (mraidScript, bideaseScript) = withContext(Dispatchers.IO) {
-            Pair(
-                scriptSource.getMraidScript(),
-                scriptSource.getBideaseScript()
-            )
+
+        var sceneResults = emptyList<AdView.LoadResult>()
+        scenes?.let { scenes ->
+            sceneResults = scenes.filterIsInstance<WebViewScene>().mapIndexed { sceneIndex, scene ->
+                val adView = AdView(
+                    context = context,
+                    scene = scene,
+                    webViewChannel = webViewChannel,
+                    shouldCloseVisible = false,
+                    createMraidJsInterface = { webView -> BannerMraidJsInterface(webView) }
+                ).apply {
+                    onClick = { _ ->
+                        println("Bidease Banner button click")
+                    }
+                    onCloseClick = ::dismiss
+                    onWebViewClick = { url ->
+                        println("Bidease Banner click")
+
+                        coroutineScope.launch {
+                            if (url != null) {
+                                context.openExternalLink(url)
+                            }
+
+                            onClicked?.invoke()
+                        }
+                    }
+                    onSwitchScene = ::switchScene
+                }
+                adView.load()
+            }
         }
 
-        this.adViews = scenes?.filterIsInstance<WebViewScene>()?.mapIndexed { sceneIndex, scene ->
-            val webView = AdWebView(context)
-            val mraid = BannerMraidJsInterface(webView)
-            mraid.onOpen = { url ->
-                println("Bidease Banner click")
-
-                if (url != null) {
-                    context.openExternalLink(url)
+        if (sceneResults.isNotEmpty()) {
+            when(val sceneRes = sceneResults.first()) {
+                is AdView.LoadSuccess -> {
+                    this.sceneResults = sceneResults
+                    switchScene(0)
+                    println("Bidease Banner loaded")
+                    onLoaded?.invoke()
+                    trackDisplay(sceneRes.view)
                 }
-
-                coroutineScope.launch {
-                    onClicked?.invoke()
-                }
-            }
-            mraid.onClose = {
-                dismiss()
-            }
-            webView.addJavascriptInterface(mraid, "jsBridge")
-            val bidease = BideaseJsInterface(
-                bideaseScript,
-                webView,
-                webViewChannel
-            )
-            bidease.onClose = {
-                dismiss()
-            }
-            bidease.onSwitchScene = ::switchScene
-            webView.addJavascriptInterface(bidease, "bideaseBridge")
-            webView.loadHtml(
-                scene.baseUrl,
-                scene.html
-            )
-
-            webView.webViewClient = MraidWebViewClient(
-                scene.mraidEnv ?: MraidEnv(),
-                mraidScript,
-                scene.timeoutMsec
-            ).apply {
-                onFinishLoading = {
-                    mraid.onStateChange(MraidJsInterface.State.DEFAULT)
-                    mraid.onReady()
-                    bidease.onReady()
-                    coroutineScope.launch {
-                        if (!isLoaded) {
-                            println("Bidease Banner loaded")
-                            isLoaded = true
-                            onLoaded?.invoke()
-                            trackDisplay(webView)
-                        }
-                    }
-                }
-                onOpenExternalLink = { url ->
-                    println("Bidease Banner click")
-
-                    if (url != null) {
-                        context.openExternalLink(url)
-                    }
-
-                    coroutineScope.launch {
-                        onClicked?.invoke()
-                    }
-                }
-                this@apply.onError = { error ->
-                    println("Bidease Banner error ${error}")
-                    coroutineScope.launch {
-                        if (!isLoaded) {
-                            onFailed?.invoke(error)
-                        }
-                    }
+                is AdView.LoadFailure -> {
+                    println("Bidease Banner error ${sceneRes.error}")
+                    onFailed?.invoke(sceneRes.error)
                 }
             }
-            webView
-        } ?: emptyList()
-        switchScene(0)
+        } else {
+            println("Bidease Banner error scenes is empty")
+            onFailed?.invoke("scenes is empty")
+        }
     }
 
     private fun fireDisplay() {
@@ -172,24 +141,27 @@ class AdmBannerView: FrameLayout {
 
     private fun switchScene(scene: Int) {
         println("Bidease Banner switchScene $scene")
-        if (scene >= 0 && scene < adViews.size) {
+        if (scene >= 0 && scene < sceneResults.size) {
             removeAllViews()
-            val view = adViews[scene]
-            val height = adSize?.height?.dpToPx(context)?.toInt()
-                ?: LayoutParams.WRAP_CONTENT
-            addView(
-                view, ViewGroup.LayoutParams(
-                    LayoutParams.MATCH_PARENT,
-                    height
+            val sceneResult = sceneResults[scene]
+            if (sceneResult is AdView.LoadSuccess) {
+                val view = sceneResult.view
+                val height = adSize?.height?.dpToPx(context)?.toInt()
+                    ?: LayoutParams.WRAP_CONTENT
+                addView(
+                    view, ViewGroup.LayoutParams(
+                        LayoutParams.MATCH_PARENT,
+                        height
+                    )
                 )
-            )
+            }
         }
     }
 
     private fun dismiss() {
         println("Bidease Banner close")
-        removeAllViews()
         coroutineScope.launch {
+            removeAllViews()
             onClosed?.invoke()
         }
     }
